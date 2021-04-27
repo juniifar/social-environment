f1c3eddffa4a21c0735845f58b9137eb847ab049
juniifar <rubahapi@gmail.com>
2021-04-27 20:39:53 +0700

    hilangin delete

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -67,45 +67,6 @@ const Login = () => {
                     Sign in on the internal platform
                   </Typography>
                 </Box>
-                {/* <Grid container spacing={3}>
-                  <Grid item xs={12} md={6}>
-                    <Button
-                      color="primary"
-                      fullWidth
-                      startIcon={<FacebookIcon />}
-                      onClick={handleSubmit}
-                      size="large"
-                      variant="contained"
-                    >
-                      Login with Facebook
-                    </Button>
-                  </Grid>
-                  <Grid item xs={12} md={6}>
-                    <Button
-                      fullWidth
-                      startIcon={<GoogleIcon />}
-                      onClick={handleSubmit}
-                      size="large"
-                      variant="contained"
-                    >
-                      Login with Google
-                    </Button>
-                  </Grid>
-                </Grid>
-                <Box
-                  sx={{
-                    pb: 1,
-                    pt: 3
-                  }}
-                >
-                  <Typography
-                    align="center"
-                    color="textSecondary"
-                    variant="body1"
-                  >
-                    or login with email address
-                  </Typography>
-                </Box> */}
                 <TextField
                   error={Boolean(touched.email && errors.email)}
                   fullWidth
